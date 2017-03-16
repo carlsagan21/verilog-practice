d58523206801d91ce9989b9f85f2f068c1242291
soo <carlsagan96@gmail.com>
2017-03-16 15:20:03 +0900

    comments IP catalog module

--- lab2/my_integer_mac.v
+++ lab2/my_integer_mac.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 03/16/2017 10:20:53 AM
-// Design Name: 
+// Design Name:
 // Module Name: my_integer_mac
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -25,22 +25,22 @@ module my_integer_mac(
     input clk,
     output [31:0] out
     );
-    
+
     wire [31:0] sum;
-        
-        
-    mult_gen_0 multi(
+
+
+    mult_gen_MAC multi( // from IP catalog
         .A(ain),
         .B(bin),
         .CLK(clk),
         .P(sum)
     );
-        
-    c_addsub_MAC adder(
+
+    c_addsub_MAC adder( // from IP catalog
         .A(sum),
         .B(cin),
         .CLK(clk),
         .S(out)
     );
-    
+
 endmodule
